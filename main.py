fc11b774bc0aeaf9a2e4486d12167b754b89ebe7
esdraslov <edvan.marcelino56@gmail.com>
2024-01-07 10:52:10 -0300

    imported librays

---
+++ main.py
@@ -0,0 +1,7 @@
+# import modules
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtPrintSupport import *
+import sys, os
