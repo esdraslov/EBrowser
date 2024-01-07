d73d2dc0fbf0aef8b9d318be33d920fd293e75f7
esdraslov <edvan.marcelino56@gmail.com>
2024-01-07 10:53:52 -0300

    set class MainWindow

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtPrintSupport import *
 import sys, os
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('https://www.google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        self.printAction = QAction('Print', self)
+        self.printAction.setShortcut('Ctrl+P')
