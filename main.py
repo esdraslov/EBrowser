c6f4611d4d9de8dfad6667c5b1f4c2090611405e
esdraslov <edvan.marcelino56@gmail.com>
2024-01-07 10:55:41 -0300

    set print action

--- main.py
+++ main.py
@@ -9,11 +9,22 @@ import sys, os
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-
+        # set browser
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl('https://www.google.com'))
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
+        # define print action
         self.printAction = QAction('Print', self)
         self.printAction.setShortcut('Ctrl+P')
+        self.printAction.triggered.connect(self.printPage)
+        self.toolbar = QToolBar()
+        self.addToolBar(self.toolbar)
+        self.toolbar.addAction(self.printAction)
+
+    def printPage(self):
+        printer = QPrinter()
+        printDialog = QPrintDialog(printer, self)
+        if printDialog.exec_() == QPrintDialog.Accepted:
+            self.browser.print_(printer)
